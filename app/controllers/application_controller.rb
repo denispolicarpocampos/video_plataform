6b894265ab8f4e4821931b1b45834168564df2d2
Denis Policarpo <campos_bra@hotmail.com>
2019-02-11 03:34:19 -0200

    Set errors of application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,23 @@
 class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound do |msg|
-    render(json: { message: msg }, status: :not_found)
+    respond_to do |format|
+      format.json { head :not_found, content_type: 'text/html' }
+      format.html { render file: "#{Rails.root}/public/404", layout: false, status: :not_found }
+    end
   end
 
   rescue_from ActionController::ParameterMissing do |exception|
-    render(json: { message: exception.param }, status: :bad_request)
+    respond_to do |format|
+      format.json { head :bad_request, content_type: 'text/html' }
+      format.html { render file: "#{Rails.root}/public/422", layout: false, status: :bad_request }
+    end
   end
 
   rescue_from CanCan::AccessDenied do |msg|
-    render(json: { message: msg}, status: :forbidden)
+    respond_to do |format|
+      format.json { head :forbidden, content_type: 'text/html' }
+      format.html { render file: "#{Rails.root}/public/403", layout: false, status: :forbidden }
+    end
   end
 end
